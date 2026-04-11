ec5171c18c1006bb508f060e79f52794728056f5
Kimly <kimlyra55@gmail.com>
2026-04-11 16:49:33 +0700

    Remove .env from tracked files and update PostsController

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -34,7 +34,7 @@ class PostsController extends Controller
         $request->validate([
             'title' => 'required|string|max:255',
             'image' => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
-            'body' => 'required|string',
+            // 'body' => 'required|string',
         ]);
 
         $data = $request->all();
